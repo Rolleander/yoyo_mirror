673699312efbc1fb66568b9ff0db62eeac810ffd
Olly Cope <olly@ollycope.com>
2020-06-08 09:40:51 +0000

    doc: switch pygments style

--- doc/conf.py
+++ doc/conf.py
@@ -76,7 +76,7 @@ language = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = "solarized-dark"
 
 
 # -- Options for HTML output -------------------------------------------------
