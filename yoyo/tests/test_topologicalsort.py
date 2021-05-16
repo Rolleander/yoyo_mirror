887052e48ea74008092fdb201e49434e267a3701
Olly Cope <olly@ollycope.com>
2021-05-16 17:34:47 +0000

    topological sort/tests: simplify edge notation and invert sense
    I find it less confusing to read "AB" as "A depends on B".

--- yoyo/tests/test_topologicalsort.py
+++ yoyo/tests/test_topologicalsort.py
@@ -8,53 +8,54 @@ from yoyo import topologicalsort
 class TestTopologicalSort(object):
     def check(self, nodes, edges, expected):
         deps = {}
+        edges = edges.split() if edges else []
         for a, b in edges:
             deps.setdefault(a, set()).add(b)
         output = list(topologicalsort.topological_sort(nodes, deps))
         for a, b in edges:
-            assert output.index(a) < output.index(b)
+            assert output.index(a) > output.index(b)
         assert output == list(expected)
 
     def test_it_keeps_stable_order(self):
         for s in map(str, itertools.permutations("ABCD")):
-            self.check(s, [], s)
+            self.check(s, "", s)
 
     def test_it_sorts_topologically(self):
 
         # Single group at start
-        self.check("ABCD", {"AB"}, "ABCD")
-        self.check("BACD", {"AB"}, "ABCD")
+        self.check("ABCD", "BA", "ABCD")
+        self.check("BACD", "BA", "ABCD")
 
         # Single group in middle start
-        self.check("CABD", {"AB"}, "CABD")
-        self.check("CBAD", {"AB"}, "CABD")
+        self.check("CABD", "BA", "CABD")
+        self.check("CBAD", "BA", "CABD")
 
         # Extended group
-        self.check("ABCD", {"AB", "AD"}, "ABCD")
-        self.check("DBCA", {"AB", "AD"}, "CADB")
+        self.check("ABCD", "BA DA", "ABCD")
+        self.check("DBCA", "BA DA", "CADB")
 
         # Non-connected groups
-        self.check("ABCDEF", {"CB", "ED"}, "ACBEDF")
-        self.check("ADEBCF", {"CB", "ED"}, "AEDCBF")
-        self.check("ADEFBC", {"CB", "ED"}, "AEDFCB")
-        self.check("DBAFEC", {"CB", "ED"}, "AFEDCB")
+        self.check("ABCDEF", "BC DE", "ACBEDF")
+        self.check("ADEBCF", "BC DE", "AEDCBF")
+        self.check("ADEFBC", "BC DE", "AEDFCB")
+        self.check("DBAFEC", "BC DE", "AFEDCB")
 
     def test_it_discards_missing_dependencies(self):
-        self.check("ABCD", "CX", "ABCD")
+        self.check("ABCD", "CX XY", "ABCD")
 
     def test_it_catches_cycles(self):
         with pytest.raises(topologicalsort.CycleError):
-            self.check("ABCD", {"AA"}, "")
+            self.check("ABCD", "AA", "")
         with pytest.raises(topologicalsort.CycleError):
-            self.check("ABCD", {"AB", "BA"}, "")
+            self.check("ABCD", "AB BA", "")
         with pytest.raises(topologicalsort.CycleError):
-            self.check("ABCD", {"AB", "BC", "CB"}, "")
+            self.check("ABCD", "AB BC CB", "")
         with pytest.raises(topologicalsort.CycleError):
-            self.check("ABCD", {"AB", "BC", "CA"}, "")
+            self.check("ABCD", "AB BC CA", "")
 
     def test_it_handles_multiple_edges_to_the_same_node(self):
-        self.check("ABCD", {"AB", "AC", "AD"}, "ABCD")
-        self.check("DCBA", {"AB", "AC", "AD"}, "ADCB")
+        self.check("ABCD", "BA CA DA", "ABCD")
+        self.check("DCBA", "BA CA DA", "ADCB")
 
     def test_it_handles_multiple_edges_to_the_same_node2(self):
         #      A --> B
@@ -62,7 +63,7 @@ class TestTopologicalSort(object):
         #      v     |
         #      C --- +
         for input_order in itertools.permutations("ABC"):
-            self.check(input_order, {"AB", "AC", "CB"}, "ACB")
+            self.check(input_order, "BA CA BC", "ACB")
 
     def test_it_doesnt_modify_order_unnecessarily(self):
         """
@@ -70,4 +71,4 @@ class TestTopologicalSort(object):
 
         https://lists.sr.ht/~olly/yoyo/%3C09c43045fdf14024a0f2e905408ea41f%40atos.net%3E
         """
-        self.check("ABC", "AC", "ABC")
+        self.check("ABC", "CA", "ABC")
