792ea2d2df61116e284772fcce81b23e7e511263
Olly Cope <olly@ollycope.com>
2022-11-03 14:55:49 +0000

    docs: enable sphinxcontrib-programoutput

--- doc/conf.py
+++ doc/conf.py
@@ -48,6 +48,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
+    "sphinxcontrib.programoutput",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
